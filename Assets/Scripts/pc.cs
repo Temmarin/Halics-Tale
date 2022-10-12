6ea289006915fc578a626c84ecf3e0eddb81e84c
Tem <taya.carlson@gmail.com>
2022-10-11 21:38:37 -0600

    Finished character movement and animations; the character can how move across the map freely, but is bounded by objects and boundaries.

--- Assets/Scripts/pc.cs
+++ Assets/Scripts/pc.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class pc : MonoBehaviour
 {
     [SerializeField] private float forceMult;
     //[SerializeField] private Rigidbody2D pcRb2d;
     [SerializeField] private SpriteRenderer pcSr;
+    [SerializeField] private SpriteRenderer shadowSr;
+    [SerializeField] private Tilemap walkable;
+    [SerializeField] private Animator animator;
+    [SerializeField] private Rigidbody2D rb2d;
     
     // Start is called before the first frame update
     void Start()
@@ -17,6 +22,9 @@ public class pc : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //var init = transform.position.magnitude;
+        //animator.SetFloat("pcSpeed", transform.position.magnitude);
+        //animator.SetFloat("pcSpeed", rb2d.velocity.magnitude);
         if (Input.GetKey(KeyCode.A))
         {
             //Move left by adding force
@@ -24,6 +32,9 @@ public class pc : MonoBehaviour
             transform.position += new Vector3(-1f, 0f, 0f) * (forceMult * Time.deltaTime);
             //Flip sprite
             pcSr.flipX = true;
+            shadowSr.flipX = false;
+            animator.SetFloat("pcSpeed", 1f);
+
         }
         if (Input.GetKey(KeyCode.D))
         {
@@ -32,18 +43,31 @@ public class pc : MonoBehaviour
             transform.position += new Vector3(1f, 0f, 0f) * (forceMult * Time.deltaTime);
             //Flip sprite
             pcSr.flipX = false;
+            shadowSr.flipX = true;
+            animator.SetFloat("pcSpeed", 1f);
+
         }
         if (Input.GetKey(KeyCode.W))
         {
             //Move up by adding force
             transform.position += new Vector3(0f, 1f, 0f) * (forceMult * Time.deltaTime);
             //pcRb2d.AddForce(transform.up * Time.deltaTime * forceMult);
+            animator.SetFloat("pcSpeed", 1f);
+
         }
         if (Input.GetKey(KeyCode.S))
         {
             transform.position += new Vector3(0f, -1f, 0f) * (forceMult * Time.deltaTime);
             //Move down by adding force
             //pcRb2d.AddForce(transform.up * Time.deltaTime * -forceMult);
+            animator.SetFloat("pcSpeed", 1f);
+
         }
+        if (!Input.anyKey)
+        {
+            animator.SetFloat("pcSpeed", 0f);
+
+        }
+        //animator.SetFloat("pcSpeed", Mathf.Abs(init - transform.position.magnitude));
     }
 }
